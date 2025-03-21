4104dcb874134b4977585dfe4d790668d460693d
Beatris Ilieve <beatrisilieve@icloud.com>
2025-03-21 20:05:50 +0200

    update

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const phrases = [
     },
     {
         Translation: 'Мога да взема само четирима пътници',
-        Bulgarian: 'Ай кен тейк фор пасенджърс',
+        Bulgarian: 'Ай кен тейк фор песинджърс',
         English: 'I can take 4 passengers'
     },
     {
@@ -33,6 +33,203 @@ const phrases = [
         Translation: 'Къде отивате?',
         Bulgarian: 'Уеър ар ю гоинг?',
         English: 'Where are you going'
+    },
+    {
+        Translation: 'Не знам къде е това място. Имате ли карта?',
+        Bulgarian: "Ай дон'т кноу дат плейс. Ду ю хав а мап?",
+        English: "I don't know that place. Do you have a map?"
+    },
+    {
+        Translation: 'Можете ли да ми покажете на тази карта?',
+        Bulgarian: 'Кен ю шоу ми он дис мап?',
+        English: 'Can you show me on this map?'
+    },
+    {
+        Translation: 'Бързате ли?',
+        Bulgarian: 'Ар ю ин ъ хъри?',
+        English: 'Are you in a hurry?'
+    },
+    {
+        Translation: 'Много е близо.',
+        Bulgarian: 'Ит ис вери клоуз.',
+        English: 'It is very close.'
+    },
+    {
+        Translation: 'Доста е далеч.',
+        Bulgarian: 'Ит ис прити фар ауей.',
+        English: 'It is pretty far away.'
+    },
+    {
+        Translation: 'Ще стигнем за около 15 мин.',
+        Bulgarian: 'Ит уил тейк абоут 15 минитс.',
+        English: 'It will take about 15 minutes.'
+    },
+    {
+        Translation: 'Ще отнеме около час да стигнем дотам.',
+        Bulgarian: 'Ит тейкс абоут уан ауър то гет деър.',
+        English: 'It takes about one hour to get there.'
+    },
+    {
+        Translation: 'Ще отнеме около час да стигнем дотам.',
+        Bulgarian: 'Ит тейкс абоут уан ауър то гет деър.',
+        English: 'It takes about one hour to get there.'
+    },
+    {
+        Translation:
+            'За извънградски превози се заплаща винетна такса.',
+        Bulgarian: 'Фор оут ъф таун трипс, а виньет фии ис рикуаърд.',
+        English: 'For out-of-town trips, a vignette fee is required.'
+    },
+    {
+        Translation: 'Говорете по-бавно моля',
+        Bulgarian: 'Спийк слоули, плийс.',
+        English: 'Speak slowly, please.'
+    },
+    {
+        Translation: 'Извинете, не Ви разбирам.',
+        Bulgarian: "Сори, ай дон'т. ъндърстанд.",
+        English: "Sorry, I don't understand."
+    },
+    {
+        Translation: 'Да, разбирам. Ще Ви закарам там.',
+        Bulgarian: "Йес, ай ъндърстанд. А'л тейк ю деър",
+        English: "Yes, I understand. I'll take you there."
+    },
+    {
+        Translation: 'Ще сложа багажа Ви в багажника.',
+        Bulgarian: "А' път йор лагидж ин дъ трънк.",
+        English: "I'll put your luggage in the trunk."
+    },
+    {
+        Translation: 'Сложете си колана.',
+        Bulgarian: 'Път он йор сийт белт, плийс.',
+        English: 'Put on your seat belt, please.'
+    },
+    {
+        Translation: 'Имате ли против да отворя прозореца?',
+        Bulgarian: 'Из ит окей иф ай оупън дъ уиндоу?',
+        English: 'Is it okay if I open a window?'
+    },
+    {
+        Translation: 'Добре ли е климатикът?',
+        Bulgarian: 'Из ди еър-къндишънинг окей?',
+        English: 'Is the air-conditioning okay?'
+    },
+    {
+        Translation: 'Моля не се хранете в колата.',
+        Bulgarian: "Плийс дон'т ийт ин дъ кар.",
+        English: "Please don't eat in the car."
+    },
+    {
+        Translation: 'Моля пушете в колата.',
+        Bulgarian: 'Но смоукийнг, плийс.',
+        English: 'No smoking, please.'
+    },
+    {
+        Translation: 'Изкате ли да Ви изчакам?',
+        Bulgarian: 'Ду ю уонт ми то уейт?',
+        English: 'Do you want me to wait?'
+    },
+    {
+        Translation: 'Трябва да заредя колата.',
+        Bulgarian: 'Ай хав то чардж дъ кар.',
+        English: 'I have to charge the car.'
+    },
+    {
+        Translation: 'Има задръстване.',
+        Bulgarian: "Ит'с ръш ауър.",
+        English: "It's rush hour."
+    },
+    {
+        Translation:
+            'Там има голям трафик. Ще стигнем по-бързо ако минем оттук.',
+        Bulgarian:
+            "Дъ трафик ис бад оувър хиър. Уи'л гет деър фастер иф уи го фром хиър.",
+        English:
+            "The traffic is bad over there. We'll get there faster if we go from here."
+    },
+    {
+        Translation: 'Мисля че има катастрофа.',
+        Bulgarian: "Ай тинк деър'с бийн ън аксидънт.",
+        English: "I think there's been an accident."
+    },
+    {
+        Translation: 'Ще опитам по друг път.',
+        Bulgarian: "Ай'л трай ъ диффърънт уей.",
+        English: "I'll try a different way."
+    },
+    {
+        Translation: 'Цената е 30лв.',
+        Bulgarian: 'Тхъ тотал прайс ис търти лев.',
+        English: 'The total price is 30lv.'
+    },
+    {
+        Translation: 'Благодаря за бакшиша.',
+        Bulgarian: 'Тенк ю фор дъ тип.',
+        English: 'Thank you for the tip.'
+    },
+    {
+        Translation: 'Имате ли по-дребни?',
+        Bulgarian: 'Ду ю хав смолър ноутс',
+        English: 'Do you have smaller notes?'
+    },
+    {
+        Translation: 'Не мога да Ви върна ресто.',
+        Bulgarian: "Ай дон'т хав ени чейндж",
+        English: "I don't have any change."
+    },
+    {
+        Translation: 'Не забравяйте вещите си.',
+        Bulgarian: "Дон'т фъргет йор тинкс.",
+        English: "Don't forget your things."
+    },
+    {
+        Translation: 'Беше ми приатно да си поговорим.',
+        Bulgarian: "Ит'с бийн найс чатинг уит ю.",
+        English: "It's been nice chatting with you."
+    },
+    {
+        Translation: 'Приатно прекарване в София.',
+        Bulgarian: 'Инждой йор стей ин Софиа.',
+        English: 'Enjoy your stay in Sofia.'
+    },
+    {
+        Translation: 'Моля, пак заповядайте.',
+        Bulgarian: 'Ю ар уелкъм.',
+        English: "You're welcome."
+    },
+    {
+        Translation:
+            'Не мога да отида там. Ще трябва да се върна без такса.',
+        Bulgarian:
+            "Ай кан'т гоу деър. И уд хав то към бак уитаут а феър.",
+        English:
+            "I can't go there. I would have to come back without a fare."
+    },
+    {
+        Translation: 'Би ли ли сте тук и преди?',
+        Bulgarian: 'Хав ю бийн хиер бифор?',
+        English: 'Have you been here before?'
+    },
+    {
+        Translation: 'Харесва ли ви Софиа?',
+        Bulgarian: 'Хау ду ю лайк Софиа?',
+        English: 'How do you like Sofia?'
+    },
+    {
+        Translation: 'По работа ли сте тук?',
+        Bulgarian: 'Ар ю хиър он бизнес?',
+        English: 'Are you here on business?'
+    },
+    {
+        Translation: 'Дълго ли ще останете?',
+        Bulgarian: 'Ар ю стеййнг лонг?',
+        English: 'Are you staying long?'
+    },
+    {
+        Translation: 'Приятен ден!',
+        Bulgarian: 'Хав а найс дей!',
+        English: 'Have a nice day!'
     }
 ];
 
