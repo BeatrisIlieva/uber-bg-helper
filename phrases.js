109a69c65bb3c15b1afa42fae0dbd204dacdbcd0
Beatris Ilieve <beatrisilieve@icloud.com>
2025-03-23 18:30:48 +0200

    Added a solution

--- phrases.js
+++ phrases.js
@@ -534,7 +534,7 @@ export const phrases = [
     {
         Translation: 'Всичко е наред.',
         Bulgarian: 'Датс окей.',
-        English: 'That’s okay.'
+        English: "That's okay."
     },
     {
         Translation: 'Извинете, говорите ли английски?',
@@ -630,6 +630,127 @@ export const phrases = [
         Translation: 'Кое време е?',
         Bulgarian: 'Уот тайм из ит?',
         English: 'What time is it?'
+    },
+    {
+        Translation:
+            'Времето днес изглежда перфекто за разглеждане на забележителности.',
+        Bulgarian: 'Тъдейс уедър сиймс пърфект фор сайтсийнг.',
+        English: "Today's wheather seems perfect for sightseeing."
+    },
+    {
+        Translation:
+            'Ако се чудите какво да правите, паркът наблизо е доста красив.',
+        Bulgarian:
+            'Иф ю аре лукинг фор съмтинг ту ду, дъ парк ниърбай ис куайт бютифъл.',
+        English:
+            'If you are looking for something to do, the park nearby is quite beautiful.'
+    },
+    {
+        Translation:
+            'Този квартал е известен с чудесните си ресторанти, планирате ли да опитате някой?',
+        Bulgarian:
+            'Дис нейбърхуд ис ноун фор итс грейт рестаурантс, ар ю планинг то чрай ени?',
+        English:
+            'This neighbordhood is known for its great restaurants, are you planning to try any?'
+    },
+    {
+        Translation:
+            'Ако се интересувате от тукашни кафенета, има някои добри близо до Вашата дестинация.',
+        Bulgarian:
+            'Иф ю ар интрестид ин лоукал кофи шопс, деър ар съм гуд уанс ниър йор дестънейшън.',
+        English:
+            'If you are interested in local coffee shops, there are some good ones near your destination.'
+    },
+    {
+        Translation:
+            'Бихте ли искали да Ви препоръчам някакви атракции близо до Вашата дестинация?',
+        Bulgarian:
+            'Удж ю лайк ми ту рикъменд ени атракшънс араунд йор дестънейшън?',
+        English:
+            'Would you like me to recommend any attractions around your destination?'
+    },
+    {
+        Translation:
+            'Тази предстояща улица е известна с магазините си и си заслужава да я посетите, ако имате време.',
+        Bulgarian:
+            'Дис ъпкъминг стрийт ис феймъс фор итс шопинг, уърф ъ визит иф ю хав тайм.',
+        English:
+            'This upcoming street is famous for its shopping, worth a visit if you have time.'
+    },
+    {
+        Translation: 'Може ли да ме закарате?',
+        Bulgarian: 'Кен ай гет ъ райд?',
+        English: 'Can I get a ride?'
+    },
+    {
+        Translation: 'Колко човека може да поберете?',
+        Bulgarian: 'Хау мени пийпъл кен ю фит?',
+        English: 'How many people can you fit?'
+    },
+    {
+        Translation: 'Колко време ще отнеме?',
+        Bulgarian: 'Хау лонг уил ит тейк?',
+        English: 'How long will it take?'
+    },
+    {
+        Translation: 'Моля, изчакайте ме.',
+        Bulgarian: 'Плийс уейт фор ми.',
+        English: 'Please wait for me.'
+    },
+    {
+        Translation: 'Може да оставите апарата да работи.',
+        Bulgarian: 'Ю кен кийп дъ митър рънинг.',
+        English: 'You can keep the meter running.'
+    },
+    {
+        Translation: 'Имате ли фиксирана цена до летището?',
+        Bulgarian: 'Ду ю хав ъ флат фий ту ди еърпорт?',
+        English: 'Do you have a flat fee to the airport?'
+    },
+    {
+        Translation: 'Малко бързам.',
+        Bulgarian: 'Айм ин ъ бит ъф ъ ръш.',
+        English: "I'm in a bit of a rush."
+    },
+    {
+        Translation: 'Може ли да стигнем там до 10 часа?',
+        Bulgarian: 'Кен уи гет деър бай тен ъклок?',
+        English: "Can we get there by 10 o'clock?"
+    },
+    {
+        Translation: 'Може ли да намалите, моля?',
+        Bulgarian: 'Кудж ю слоу даун, плийс?',
+        English: 'Could you slow down, please?'
+    },
+    {
+        Translation: 'Това най-бързият път ли е?',
+        Bulgarian: 'Из дис дъ куикест уей?',
+        English: 'Is this the quickest way?'
+    },
+    {
+        Translation: 'Приемате ли кредитни карти?',
+        Bulgarian: 'Ду ю тейк кредит кардс?',
+        English: 'Do you take credit cards?'
+    },
+    {
+        Translation: 'Имате ли дребни?',
+        Bulgarian: 'Ду ю хав чейндж?',
+        English: 'Do you have change?'
+    },
+    {
+        Translation: 'Запазете рестото.',
+        Bulgarian: 'Кийп дъ чейндж?',
+        English: 'Keep the change.'
+    },
+    {
+        Translation: 'Благодаря за превоза.',
+        Bulgarian: 'Тенкс фор дъ райд.',
+        English: 'Thanks for the ride.'
+    },
+    {
+        Translation: 'Благодаря за превоза.',
+        Bulgarian: 'Тенкс фор дъ лифт.',
+        English: 'Thanks for the lift.'
     }
 ];
 
@@ -648,13 +769,13 @@ export const dictionary = {
     passengers: 'пътници',
     where: 'къде',
     going: 'текущо време на глогола отивам',
-    "don't": 'съкратена форма на `do not`',
+    "don't": "съкратена форма на 'do not'",
     know: 'знам',
     that: 'това/онова',
     place: 'място',
     do: 'правя',
     have: 'имам',
-    a: 'неопределителен член -> `I saw a cat.(Видях котка.)`',
+    a: "неопределителен член -> 'I saw a cat.(Видях котка.)'",
     map: 'карта',
     show: 'показвам',
     me: 'ми/ме',
@@ -690,11 +811,11 @@ export const dictionary = {
     sorry: 'съжалявам',
     understand: 'разбирам',
     yes: 'да',
-    "i'll": 'съкратена форма на `I will` (Аз ще)',
+    "i'll": "съкратена форма на 'I will' (Аз ще)",
     put: 'слагам',
     your: 'твоят/твое/твоята/твоето',
     luggage: 'багаж',
-    the: 'определителен член -> `I saw the cat to ruin the curtains.(Видях котката да съсипва пердетата.)`',
+    the: "определителен член -> 'I saw the cat to ruin the curtains.(Видях котката да съсипва пердетата.)'",
     trunk: 'багажник',
     seat: 'седалка',
     belt: 'колан',
@@ -710,19 +831,19 @@ export const dictionary = {
     want: 'искам',
     wait: 'чакам',
     charge: 'зареждам',
-    "it's": 'съкратена форма на `it is` (то е)',
+    "it's": "съкратена форма на 'it is' (то е)",
     rush: 'бързам/бързо',
     traffic: 'трафик',
     bad: 'лошо',
     over: 'по/през/за',
-    "we'll": 'съкратена форма на `we will` (ние ще)',
+    "we'll": "съкратена форма на 'we will' (ние ще)",
     get: 'вземам',
     faster: 'по-бързо',
     we: 'ние',
     from: 'от',
     here: 'тук',
     think: 'мисля',
-    "there's": 'съкратена форма на `there is` (там има)',
+    "there's": "съкратена форма на 'there is' (там има)",
     been: 'минало време на глагола съм',
     an: 'неопределителен член за думи започващи с гласна',
     accident: 'инцидент',
@@ -744,9 +865,9 @@ export const dictionary = {
     with: 'със',
     enjoy: 'забавлявам се',
     stay: 'оставам/стоя',
-    "you're": 'съкратена форма на `you are` (ти си)',
+    "you're": "съкратена форма на 'you are' (ти си)",
     welcome: 'моля/добре дошъл',
-    "can't": 'съкратена форма на `cannot` (немога)',
+    "can't": "съкратена форма на 'cannot' (немога)",
     would: 'бих',
     come: 'идвам',
     back: 'отзад/назад',
@@ -776,7 +897,7 @@ export const dictionary = {
     so: 'така/толкова',
     rest: 'останал/останала/останало',
     of: 'на (принадлежи на)',
-    "i've": 'съкратена форма на `I have` (Аз имам)',
+    "i've": "съкратена форма на 'I have' (Аз имам)",
     just: 'точно',
     arrived: 'пристигнах',
     at: 'на (на летището)',
@@ -805,13 +926,14 @@ export const dictionary = {
     find: 'намирам',
     safe: 'безопасен',
     and: 'и',
+    drop: 'падам',
     assist: 'помагам',
     taking: 'вземайки',
     belongings: 'принадлежности',
     out: 'навън',
     adjust: 'нагласям',
     music: 'музика',
-    am: 'съм (за първо лице `I am` -> Аз съм)',
+    am: "съм (за първо лице 'I am' -> Аз съм)",
     currently: 'в момента',
     following: 'следвайки',
     but: 'но',
@@ -819,7 +941,7 @@ export const dictionary = {
     route: 'маршрут',
     guide: 'напътствам/напътствие',
     quickest: 'най-бързият',
-    "threre's": 'съкратена форма на `there is` (там има)',
+    "threre's": "съкратена форма на 'there is' (там има)",
     ahead: 'напред',
     alternative: 'алтернативен',
     should: 'трябва',
@@ -870,8 +992,8 @@ export const dictionary = {
     slower: 'по-бавно',
     mean: 'имам предвид',
     cost: 'разход/цена',
-    "that's": 'съкратена форма на `that is` (това е)',
-    "i'm": 'съкратена форма на `I am` (Аз съм)',
+    "that's": "съкратена форма на 'that is' (това е)",
+    "i'm": "съкратена форма на 'I am' (Аз съм)",
     heading: 'посока/направление',
     mind: 'мнение/ум',
     smoke: 'пуша',
@@ -888,5 +1010,49 @@ export const dictionary = {
     my: 'моят',
     pleasure: 'удоволствие',
     idea: 'идея',
-    drop: 'падам'
+    "today's":
+        'днешното (today -> днес, `s -> на; поставяйки апостроф след съществително оказваме принадлежност)',
+    wheather: 'време (от синоптична гледна точка)',
+    seems: 'изглежда',
+    perfect: 'перфектно',
+    sightseeing: 'забележителности',
+    looking: 'изглеждайки',
+    something: 'нещо',
+    park: 'парк',
+    nearby: 'наблизо',
+    quite: 'доста',
+    beautiful: 'красив/красива/красиво',
+    neighbordhood: 'квартал',
+    known: 'познат (минало време на глагола зная)',
+    its: 'негово',
+    restaurants: 'ресторанти',
+    planning: 'планирайки',
+    interested: 'заинтересован',
+    coffee: 'кафе',
+    shops: 'магазин',
+    ones: 'такива',
+    near: 'близо',
+    recommend: 'препоръчвам',
+    attractions: 'атракции',
+    around: 'наоколо',
+    upcoming: 'предстоящ',
+    street: 'улица',
+    famous: 'известен',
+    shopping: 'пазаруване',
+    worth: 'струвам/струва/струват',
+    visit: 'посещавам',
+    ride: 'карам',
+    many: 'много',
+    people: 'хора',
+    fit: 'побирам се',
+    keep: 'пазя',
+    meter: 'метър/апарат',
+    running: 'тичайки/работейки',
+    flat: 'плосък',
+    airport: 'летище',
+    bit: 'малко',
+    by: 'чрез, покрай',
+    "o'clock": 'час (At 10 o`clock -> В 10 часа.)',
+    slow: 'бавно',
+    lift: 'превоз'
 };
